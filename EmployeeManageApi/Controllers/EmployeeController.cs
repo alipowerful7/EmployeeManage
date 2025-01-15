e5b19532b19d1c403d7dc3b72b80b4e8c286dbe8
Ali <ali7.khosrojerdi@gmail.com>
2025-01-15 23:12:20 +0330

    add: dataBase and employee controller

---
+++ EmployeeManageApi/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManageApi.Models.DbContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManageApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ EmployeeManageApi/Models/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManageApi.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManageApi.Models.DbContext
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
